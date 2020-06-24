c12cb357d243448bbead0c969fe6ab7da615fdc2
William Chen <seed45699@gmail.com>
2020-06-24 11:35:30 +0800

    看參考範例後的訂正

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,21 +1,24 @@
 function join(arr, concatStr) {
-  return arr.reduce((prev, curr, index) => {
-    if (index < arr.length - 1) {
-      prev += curr + concatStr;
-    } else {
-      prev += curr;
-    }
+  if (arr.length === 0) return "";
+  return arr.reduce((prev, curr) => {
+    prev += concatStr + curr;
     return prev;
-  }, "");
+  });
 }
 
 function repeat(str, times) {
-  var ans = "";
-  for (var i = 1; i <= times; i++) {
+  let ans = "";
+  for (let i = 1; i <= times; i++) {
     ans += str;
   }
   return ans;
 }
 
-console.log(join(["a"], "!"));
+console.log(
+  join([], "!"),
+  join(["a"], "!"),
+  join([1, 2, 3], ""),
+  join(["a", "b", "c"], "!"),
+  join(["aaa", "bb", "c", "dddd"], ",,")
+);
 console.log(repeat("a", 5));
