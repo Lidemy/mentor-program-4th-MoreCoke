d539b77ba9cf8ddf333cb96886df8a4a72d22443
William Chen <seed45699@gmail.com>
2020-11-15 13:37:45 +0800

    將自己 week12 hw1 的作業改成 plugin

---
+++ homeworks/week13/hw2_personal/src/index.js
@@ -0,0 +1,34 @@
+import $ from 'jquery';
+
+import { getCommentData, postCommentData } from './api';
+import { formTemplate } from './template';
+
+function init(container, siteKey) {
+  $(container).append(formTemplate(siteKey));
+  const commentsDOM = $(`#${siteKey} .comments`);
+  const nicknameDOM = $(`#${siteKey} input[name="nickname"]`);
+  const contentDOM = $(`#${siteKey} textarea[name="content"]`);
+  const addCommentDOM = $(`#${siteKey} .add-comment-form`);
+  const btnLoadDOM = $(`#${siteKey} .btn-load`);
+  let limit = 5;
+
+  addCommentDOM.submit((e) => {
+    e.preventDefault();
+    postCommentData(commentsDOM, {
+      siteKey: siteKey,
+      nicknameDOM,
+      contentDOM,
+    });
+  });
+
+  btnLoadDOM.click(() => {
+    limit += 5;
+    getCommentData(commentsDOM, siteKey, limit);
+  });
+  getCommentData(commentsDOM, siteKey, limit);
+}
+
+$(document).ready(() => {
+  init('.container', 'morecoke');
+  init('.container', 'morecoke1');
+});
