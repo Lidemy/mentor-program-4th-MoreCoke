ba842f4bb6ec38aad8441e90fb6042fc163120ee
William Chen <seed45699@gmail.com>
2021-02-24 21:46:27 +0800

    Refactor file structure

--- homeworks/week21/hw1/src/index.js
+++ homeworks/week21/hw1/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import TodoList from './TodoList';
+import 'index.css';
+import { ResetStyle } from 'components/globalStyle';
+import TodoList from 'views/TodoList';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ResetStyle />
     <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ homeworks/week21/hw1/src/views/TodoList/components/TodoItem/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/jsx-one-expression-per-line */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  TodoItemWrapper,
+  TodoItemContent,
+  TodoItemCheckbox,
+  TodoItemEditInput,
+  TodoItemLabel,
+  TodoItemButtons,
+} from 'views/TodoList/components/TodoItem/style';
+import { WarningButton, DangerButton } from 'components/Button';
+
+export default function TodoItem({
+  todo,
+  onEditTodoClick,
+  onDeleteTodoClick,
+  onToggleDoneTodoClick,
+}) {
+  const [todoEditInputValue, setTodoEditInputValue] = useState(todo.todo);
+
+  const onEditTodoChange = (e) => {
+    setTodoEditInputValue(e.target.value);
+  };
+
+  return (
+    <TodoItemWrapper>
+      <div className="d-flex align-items-baseline">
+        <TodoItemContent onClick={() => onToggleDoneTodoClick(todo.id)}>
+          {/* 練習用 styled-component attr */}
+          <TodoItemCheckbox defaultChecked={todo.isDone} id={todo.id} />
+          <TodoItemLabel htmlFor={todo.id}>{todo.todo}</TodoItemLabel>
+        </TodoItemContent>
+        {todo.isEdit && (
+          // 不透過 attr 直接傳 input 也行
+          <TodoItemEditInput
+            type="text"
+            value={todoEditInputValue}
+            onChange={onEditTodoChange}
+          />
+        )}
+      </div>
+
+      <TodoItemButtons>
+        <WarningButton
+          onClick={() => onEditTodoClick(todo.id, todoEditInputValue)}
+        >
+          {todo.isEdit ? '完成編輯' : '編輯'}
+        </WarningButton>
+        <DangerButton onClick={() => onDeleteTodoClick(todo.id)}>
+          刪除
+        </DangerButton>
+      </TodoItemButtons>
+    </TodoItemWrapper>
+  );
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    todo: PropTypes.string.isRequired,
+    isDone: PropTypes.bool.isRequired,
+    isEdit: PropTypes.bool.isRequired,
+  }),
+  onEditTodoClick: PropTypes.func,
+  onDeleteTodoClick: PropTypes.func,
+  onToggleDoneTodoClick: PropTypes.func,
+};
